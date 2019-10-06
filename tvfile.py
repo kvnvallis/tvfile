49ea486b789461cac81d4e158b15cec208cf0ee3
kvn <>
2019-10-05 23:55:48 -0700

    Match a search string to episode titles
    First try a specific match by using the whole phrase, then a broad match by checking against each word.

--- tvfile.py
+++ tvfile.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# TODO: find out best practices for storing tokens and credentials, so that you don't immediately pollute your git history. It's about time to make an initial commit.
-
 import sys
 import requests
 import json
 import argparse
+import json
+import string
 
 from requests.exceptions import HTTPError
 
@@ -57,6 +57,13 @@ def find_series(search):
     return response
 
 
+def get_episodes(series_id):
+    url = 'https://api.thetvdb.com/series/{}/episodes'.format(series_id)
+    headers = {'Authorization': 'Bearer {}'.format(TOKEN)}
+    response = requests.get(url, headers=headers)
+    return response
+
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
@@ -102,10 +109,71 @@ def main():
             chosen_integer = int(choice)
             if chosen_integer <= 0:
                 raise ValueError
-            chosen_result = results_list[chosen_integer - 1]
-            print('You have selected "{}"'.format(chosen_result['seriesName']))
         except (ValueError, IndexError):
             print("Does not match any available choices")
+
+        series_data = results_list[chosen_integer - 1]
+        print('You have selected "{}"'.format(series_data['seriesName']))
+
+        series_id = series_data['id']
+        episode_list = get_episodes(series_id).json()['data']
+
+        # Collect ids and names in a list of tuples
+        #id_episode_names = list()
+        #for episode_data in episode_list:
+        #    id_episode_names.append(
+        #        (episode_data['id'], episode_data['episodeName'].lower()))
+
+        # Used as argument to translate() to remove punctuation from titles
+        table = str.maketrans('', '', string.punctuation)
+
+        # Make a dictionary of (sanitized) episode titles to episode IDs
+        episode_names_ids = dict()
+        for episode_data in episode_list:
+            episode_names_ids[episode_data['episodeName'].lower().translate(table)] = episode_data['id']
+
+        for k, v in episode_names_ids.items():
+            print(episode_names_ids)
+
+        # Make user input lowercase then remove punctuation and split words into a list
+        text = input('Enter an episode title: ').lower()
+        title = text.translate(table)
+        words_in_title = title.split()
+        #print(words_in_title)
+
+        # First try to match the whole search string to a title. 
+        phrase_matches = list()
+        for episode_name in episode_names_ids:
+            if title in episode_name:
+                phrase_matches.append(episode_name)
+        if phrase_matches:
+            print(phrase_matches)
+        # Perform a broad search by checking each word in the search string
+        # against each word in the title. This is helpful when the search
+        # string contains an error, e.g. spelling mistake. If you don't use a
+        # set(), this matches the same title multiple times, and returns
+        # duplicate titles equal to the number of words matched from the search
+        # string. 
+        else:
+            broad_matches = set()
+            for word in words_in_title:
+                for episode_name in episode_names_ids:
+                    if word in episode_name:
+                        broad_matches.add(episode_name)
+            print(broad_matches)
+
+        # This will of course only show exact matches
+        #if name in episode_names:
+        #    print("Wow we found your episode")
+
+        # Slightly better partial string search
+        #matches = list()
+        #for name in episode_names:
+        #    if name.find(title) >= 0:
+        #        matches.append(name)
+
+        #print('\n'.join(episode_names))
+        #print(json.dumps(episode_list.json(), indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
